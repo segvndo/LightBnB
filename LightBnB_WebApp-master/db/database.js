4be774262e7d4ab13383337ddbec477657419e7c
Bea Mungcal <beamungcal@hotmail.com>
2023-06-01 13:30:59 -0400

    Refactor getAllReservations in database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -80,8 +80,24 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+    SELECT properties.*, reservations.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN reservations ON properties.id = reservations.property_id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id= $1 AND reservations.start_date > Now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY start_date
+    LIMIT $2`;
+  
+    return pool
+      .query(queryString, [guest_id, limit])
+      .then(result => result.rows)
+      .catch(err => {
+        console.log(err.message);
+      });
 };
+
 
 /// Properties
 
